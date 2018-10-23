79adfc2a8e32552939ab79a4babb875ab0592c86
liyanzhe <liyanzhe@igengmei.com>
2018-10-24 00:24:05 +0800

    微博project

--- li_pro/app2/urls.py
+++ li_pro/app2/urls.py
@@ -1,7 +1,9 @@
 from app2.views import Test
 from django.conf.urls import url
-
+from app2.view.weibo import SearchInfo, Login
 
 urlpatterns = [
-    url('test/', Test.as_view())
+    url('test/', Test.as_view()),
+    url('search/weibo', SearchInfo.as_view()),
+    url('login', Login.as_view()),
 ]
--- vue_manage/src/config/env.js
+++ vue_manage/src/config/env.js
@@ -1,20 +1,20 @@
 /**
  * 配置编译环境和线上环境之间的切换
- * 
+ *
  * baseUrl: 域名地址
  * routerMode: 路由模式
  * baseImgPath: 图片存放地址
- * 
+ *
  */
-let baseUrl = ''; 
+let baseUrl = '//elm.cangdu.org';
 let routerMode = 'hash';
-let baseImgPath;
+let baseImgPath = '//elm.cangdu.org/img/';
 
 if (process.env.NODE_ENV == 'development') {
 	baseUrl = '';
     baseImgPath = '/img/';
 }else{
-	baseUrl = '//elm.cangdu.org';
+	baseUrl = '//127.0.0.1:6666';
     baseImgPath = '//elm.cangdu.org/img/';
 }
 
